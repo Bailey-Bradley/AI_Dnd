f1075249eff207cee6d88874837ae8c9350740c0
Baebird <dapperwbs@gmail.com>
2026-01-22 23:53:29 -0500

    feat: Implement LayerQuery

--- GameLayer.py
+++ GameLayer.py
@@ -1,16 +1,28 @@
 from GameObject import GameObject
 import pygame
+class LayerQuery:
+    
+    def __init__(self, objects):
+        self.objects = objects
+
+    def findObject(self, name):
+        for object in self.objects:
+            if object.name == name:
+                return object
+
+        return None
 
 class GameLayer:
     def __init__(self):
         self.objects: list[GameObject] = []
         self.renderer = None
         self.event_busses = None
-        self.layer_query = None
+        self.layer_query: LayerQuery = LayerQuery(self.objects)
 
     def addObject(self, game_object: GameObject):
         self.objects.append(game_object)
 
+        game_object.layer_query = self.layer_query
         game_object.onConnect()
 
     def removeObject(self, game_object: GameObject):
