6a04900bab10a138e0859203a279b243f01503d6
Baebird <dapperwbs@gmail.com>
2026-01-23 23:50:06 -0500

    feat: Add Serialization to EventBus

--- Events.py
+++ Events.py
@@ -1,8 +1,9 @@
+import Serialize
 import jsonpickle
 from jsonpickle.handlers import BaseHandler
 
 
-class EventBus:
+class EventBus(Serialize.Serializable):
     def __init__(self):
         self._handlers: dict[int, list[callable]] = {}
 
@@ -26,3 +27,38 @@ class EventBus:
         if handlers is not None:
             for handler in handlers:
                 handler(event)
+
+    def __getstate__(self):
+        state = {}
+        state["state"] = self.__dict__
+        serial_handlers_dict: dict[str, list[(object, str)]] = {}
+
+        for event_id, handler_list in self._handlers.items():
+            serial_handler_list = []
+
+            for event_handler in handler_list:
+                bound_object = event_handler.__self__
+                handler_name = event_handler.__qualname__.split('.')[1]
+
+                serial_handler_list.append((bound_object, handler_name))
+
+            serial_handlers_dict[str(event_id)] = serial_handler_list
+
+        state["serial_handlers"] = serial_handlers_dict
+
+        return state
+
+    def __setstate__(self, state):
+        self.__dict__ = state["state"]
+
+        self._handlers = {}
+
+        for event_id, serial_handler_list in state["serial_handlers"].items():
+            handler_list = []
+
+            for serial_handler in serial_handler_list:
+                obj = serial_handler[0]
+                method = getattr(obj, serial_handler[1])
+                handler_list.append(method)
+
+            self._handlers[int(event_id)] = handler_list
