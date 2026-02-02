7b6ef143ab376d1798bd4d1a114e8c88f1415fac
Baebird <dapperwbs@gmail.com>
2026-02-01 19:03:12 -0500

    feat: Add global event bus processing and add audio tests

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ from engine.GameLayer import GameLayer
 from engine.GameLayerStack import GameLayerStack
 from engine.Renderer import ERenderLayer
 import random
-import engine.Serialize
 from engine.LayerManager import LayerManager
+from engine.Events import EventBus, UserEvent
+from engine.Audio import AudioManager, AudioPlayer
+from engine.Resources import ResourceManager
+from pathlib import Path
+
 
 class DefaultObj(GameObject):
 
@@ -21,6 +25,8 @@ class DefaultObj(GameObject):
 
     def onConnect(self):
         self.event_bus.subscribe(pygame.KEYDOWN, self.nudge)
+        background = self.layer_query.findObject("Background")
+        self.hierarchy.addChild(background, self)
 
     def update(self):
         self.ticker += 1
@@ -28,23 +34,21 @@ class DefaultObj(GameObject):
         self.guy = pygame.transform.scale(self.guy, tuple(x * (math.sin(self.ticker * 0.1) + 2) for x in self.size))
 
     def nudge(self, event):
-        if event.unicode == 'd':
-            self.x += 10
-            background = self.layer_query.findObject("Background")
-            if background is None:
-                print("not found")
-            else:
+        match event.unicode:
+            case 'd':
+                self.x += 10
+                background = self.hierarchy.getParent(self)
                 background.surface.fill((0,random.randrange(100),0))
-        elif event.unicode == 'a':
-            self.x -= 10
-        elif event.unicode == 'w':
-            self.y -= 10
-        elif event.unicode == 's':
-            self.y += 10
-        elif event.unicode == 'u':
-            LayerManager.saveLayer()
-        elif event.unicode == 'o':
-            LayerManager.replaceLayer("quicksave")
+            case 'a':
+                self.x -= 10
+            case 'w':
+                self.y -= 10
+            case 's':
+                self.y += 10
+            case 'u':
+                LayerManager.saveLayer()
+            case 'o':
+                LayerManager.replaceLayer("quicksave")
 
     def render(self, surface: pygame.Surface) -> None:
         surface.blit(self.guy, (self.x, self.y))
@@ -74,29 +78,45 @@ class Background(GameObject):
 
 
 pygame.init()
+pygame.mixer.init()
 
 main_window = pygame.display.set_mode((1000,750), pygame.RESIZABLE)
 clocky = pygame.time.Clock()
+global_bus = EventBus()
+AudioManager.init(global_bus)
+ResourceManager.init(Path(__file__).parent)
+
+layer_stack = GameLayerStack(main_window)
+layer_manager = LayerManager(layer_stack)
 
 layer1 = GameLayer()
 
 obj1 = DefaultObj()
 obj2 = Background(main_window.get_size(), (0,2,0))
 
-layer1.addObject(obj1)
 layer1.addObject(obj2, ERenderLayer.BACKGROUND)
+layer1.addObject(obj1)
 
-layer_stack = GameLayerStack(main_window)
 layer_stack.addLayer(layer1)
 
-LayerManager(layer_stack)
+audio_player = AudioPlayer()
+
+audio_player.playMusic(["yell", "boing"])
 
 running = True
 while running:
 
-    layer_stack.update()
+    events = pygame.event.get()
+
+    for event in events:
+        if event.type == pygame.KEYDOWN:
+            if event.unicode == 'p':
+                audio_player.playSound("yell")
+
+        global_bus.emit(event.type, event)
+
+    layer_stack.update(events)
 
     clocky.tick(40)
 
     pygame.display.flip()
-    
